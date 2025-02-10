c6d86922a5278ea284271d3a32201755aff20f82
SkyVojager <andreycom068@proton.me>
2025-02-10 15:02:57 -0800

    ViewExpenses errormessage

--- widgets/view_expenses.py
+++ widgets/view_expenses.py
@@ -1,6 +1,7 @@
 from textual import on
-from textual.app import App, ComposeResult, RenderResult
+from textual.app import App, ComposeResult
 from textual.containers import Grid, Container, HorizontalGroup, VerticalGroup, VerticalScroll
+from textual.validation import Function, Number, ValidationResult, Validator
 from textual.reactive import reactive
 from textual.screen import Screen, ModalScreen
 from textual.widget import Widget
@@ -24,7 +25,12 @@ class AddExpense(Screen):
         yield Footer()
         with VerticalGroup():
             self.expense_input = Input(placeholder="Expense")
-            self.amount_input = Input(placeholder="Amount")
+            self.amount_input = Input(
+                placeholder="Amount",
+                validators=[
+                    Function(is_integer)
+                    ]
+                )
             self.description_input = Input(placeholder="Description")
             self.date_input = Input(placeholder="Date (year-month-day)") #TODO: Add a date picker
             yield self.expense_input
@@ -36,23 +42,32 @@ class AddExpense(Screen):
             yield Button("Add", classes="add_expense", id="add_expense_button")
             yield Button("Return", classes="return_button add_expense")
 
+
     @on(Button.Pressed, "#add_expense_button")
-    def add_expense(self, event: Button.Pressed) -> None:
+    def check_expense(self, event: Button.Pressed) -> None:
         
-        new_expense = {
+        if is_integer(self.amount_input.value) == False:
+            self.app.push_screen(ErrorScreen())
+        else:
+            self.add_expense()
+
+        
+    def add_expense(self):
+        
+        expense = {
             "name": self.expense_input.value,
             "amount": float(self.amount_input.value),
             "description": self.description_input.value,
             "date": self.date_input.value
         }
-
+        
         with open('user_data/expenses.json', 'r') as file:
             data = json.load(file)
 
         if self.category_name not in data['categories']:
             data['categories'][self.category_name] = []
 
-        data['categories'][self.category_name].append(new_expense)
+        data['categories'][self.category_name].append(expense)
 
         with open('user_data/expenses.json', 'w') as file:
             json.dump(data, file, indent=4, sort_keys=True, separators=(',', ': ')) #I swear i did not copied the code down below from the DeleteExpense class, okay i did.
@@ -60,23 +75,17 @@ class AddExpense(Screen):
         self.app.pop_screen()
         self.app.push_screen(ViewExpenses())
 
-
-
-
 class DeleteExpense(ModalScreen):
     """A widget to confirm deletion of an expense."""
 
-    category_name = reactive("this category", recompose=True)
-    expense_name = reactive("this expense", recompose=True) # yes, formatted like this
+    category_name = reactive("this category")
+    expense_name = reactive("this expense") # yes, formatted like this
 
-
-    
     def compose(self) -> ComposeResult:
-
         with Static(f"Are you sure you want to delete [bold italic]{self.expense_name}[/]?", id="delete_confirmation_static"):
             yield Button("Yes", variant="error", id="confirm_delete")
             yield Button("No", variant="primary", id="cancel_delete")
-        
+
     @on(Button.Pressed, "#confirm_delete")
     def confirm_delete(self, event: Button.Pressed) -> None:
         """Delete an expense."""
@@ -96,33 +105,23 @@ class DeleteExpense(ModalScreen):
                 json.dump(data, file, indent=4, sort_keys=True, separators=(',', ': '))
         self.app.pop_screen()
         self.app.push_screen(ViewExpenses()) # dammit, this is a hacky way to update the ViewExpenses screen
+        
+class ErrorScreen(Screen):
     
-
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Label("Error: Invalid input")
+        yield Button("Return", classes="return_button")
+        
+    
 class ViewExpenses(Screen):
 
     def compose(self) -> ComposeResult:
         
         yield Header()
-        
         yield Footer()
         yield VerticalScroll()
-        yield TotalExpenses()
-
-
-        # TODO: have this in TotalExpenses class and not here.
-        with Static("Total Expenses", classes="total_expenses"):
-
-            with open('user_data/expenses.json', 'r') as file:
-                data = json.load(file)
-                total_expenses: float = 0
-
-                for category in data['categories']:
-                        for expense in data['categories'][category]:
-                            total_expenses += expense['amount']
-            
-            
-            yield Digits(str(total_expenses), id="total_expense_digits")
-
 
         # Load the expenses from the JSON file
         with open('user_data/expenses.json', 'r') as file:
@@ -144,27 +143,14 @@ class ViewExpenses(Screen):
                     
 
         yield Button("Return", classes="return_button")
-
-        
+    
+    
+def is_integer(value: str) -> bool:
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
 
     def on_mount(self) -> None:
         self.title = "Your Expenses"
-
-
-
-class TotalExpenses(Static):
-    """A widget to display the total expenses."""
-
-    def compose(self) -> ComposeResult:
-        with Static("Total Expenses", classes="total_expenses"):
-
-            with open('user_data/expenses.json', 'r') as file:
-                data = json.load(file)
-                total_expenses: float = 0
-
-                for category in data['categories']:
-                        for expense in data['categories'][category]:
-                            total_expenses += expense['amount']
-            
-            
-            yield Digits(str(total_expenses), id="total_expense_digits")
